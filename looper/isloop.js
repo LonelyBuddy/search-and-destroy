371cb43933c2c15f13e5c12b7d3b2992321fbc3b
Henry Zheng <henryzheng92@gmail.com>
2019-01-22 17:27:22 -0500

    finished LL

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,8 +1,23 @@
 'use strict';
 
 //Complete this algo
-const isLoop = (linkedlist) => {
+const isLoop = (ll) => {
+  let memo = [];
 
+  while (ll.head.next !== null) {
+    // if current head.value is in memo  return true
+    if (memo.indexOf(ll.head.value) !== -1) {
+      return true;
+    }
+    // else it's not in memo
+    else {
+      // push current head.value to memo
+      memo.push(ll.head.value);
+      // assign the current head to be the head.value
+      ll.head = ll.head.next;
+    }
+  }
+  return false;
 };
 
 
